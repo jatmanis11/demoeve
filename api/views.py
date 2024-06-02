200ce3fa56bf64b448a59e4485e95e6caa81f66e
manish jat <162445465+jatmanis11@users.noreply.github.com>
2024-06-03 00:57:35 +0530

    Update views.py

--- api/views.py
+++ api/views.py
@@ -78,7 +78,7 @@ def ee(request):
 
 
 def detail(request, id1):
-        id1=id1
+    id1=id1
     headers={
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
     }
