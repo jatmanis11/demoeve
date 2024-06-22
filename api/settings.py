543ff47a99d68b04a3fe96cd29245e1aab034b90
manish jat <162445465+jatmanis11@users.noreply.github.com>
2024-06-22 15:55:42 +0530

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import psycopg2
+import dj_database_url
+from decouple import Config
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
