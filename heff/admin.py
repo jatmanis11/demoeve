469fb3b51ddb9c38bf154d81bb562f5bc7446540
manish jat <162445465+jatmanis11@users.noreply.github.com>
2024-06-28 13:11:47 +0530

    Add files via upload

--- heff/admin.py
+++ heff/admin.py
@@ -1,15 +1,23 @@
 from django.contrib import admin
 from django.db.models import Sum
-from .models import Player, Team, Match, PlayerScore
+from .models import Player, Team, Match, PlayerScore#, MatchScore
 
 class PlayerScoreInline(admin.TabularInline):
     model = PlayerScore
     extra = 1
+"""class MatchScoreInline(admin.TabularInline):
+    model = MatchScore
+    extra = 1"""
 
 class PlayerAdmin(admin.ModelAdmin):
-    list_display = ('ign', 'uid', 'email', 'phone_no', 'college_name', 'team', 'total_tournament_score')
+    list_display = ("player_name",'team','ign', 'uid' ,"display_match_scores", 'total_tournament_score', 'email', 'phone_no', 'college_name')
     search_fields = ('ign', 'uid', 'email', 'team__name')
     inlines = [PlayerScoreInline]
+
+    def display_match_scores(self, obj):
+        match_scores = obj.match_scores()
+        return ", ".join([f"{score.match.name}: {score.score}" for score in match_scores])
+    display_match_scores.short_description = 'Match Scores'
 
     def total_tournament_score(self, obj):
         return obj.total_tournament_score()
@@ -21,12 +29,24 @@ class PlayerInline(admin.TabularInline):
     max_num = 6
 
 class TeamAdmin(admin.ModelAdmin):
-    list_display = ('name', 'total_score', "player_list")
+    list_display = ('name', 'total_score',"get_matches_participated", "player_list", "display_match_scores")
     inlines = [PlayerInline]
+    def get_matches_participated(self, obj):
+        return ", ".join([match.name for match in obj.match_pp.all()])
 
+    get_matches_participated.short_description = 'Matches Participated'
     def get_queryset(self, request):
         qs = super().get_queryset(request)
         return qs.annotate(total_score=Sum('players__scores__score'))
+    def player_list(self, obj):
+        return obj.player_list()
+    player_list.short_description = 'Players'
+
+    def display_match_scores(self, obj):
+        match_scores = obj.match_scores()
+        return "\n ".join([f"{score['match__name']}: {score['team_score']} " for score in match_scores])
+    display_match_scores.short_description = 'Match Scores'
+
 
     def total_score(self, obj):
         return obj.total_score
@@ -40,8 +60,17 @@ class MatchAdmin(admin.ModelAdmin):
 class PlayerScoreAdmin(admin.ModelAdmin):
     list_display = ('player', 'match', 'score')
     search_fields = ('player__ign', 'match__name')
+"""class matchscoreAdmin(admin.ModelAdmin):
+    list_display= ("player","match")
+"""
+"""class MatchScoreAdmin(admin.ModelAdmin):
+    list_display = ('match', 'team', 'score')
+    search_fields = ('match__name', 'team__name')"""
+
+
 
 admin.site.register(Team, TeamAdmin)
 admin.site.register(Player, PlayerAdmin)
 admin.site.register(Match, MatchAdmin)
 admin.site.register(PlayerScore, PlayerScoreAdmin)
+#admin.site.register(MatchScore, MatchScoreAdmin)
--- heff/models.py
+++ heff/models.py
@@ -1,12 +1,38 @@
 from django.db import models
 from django.db.models import Sum
 
-
+class Match(models.Model):
+    name = models.CharField(max_length=100)
+    date = models.DateField()
+    #class_instance = models.ForeignKey(Class, on_delete=models.CASCADE, related_name='objects')
+    def __str__(self):
+        return self.name
 class Team(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    
+    match_pp = models.ManyToManyField(Match, related_name='teams_participated', blank=True)
 
-    def total_score(self):
+    def update_matches_participation(self):
+        from django.db.models import Count
+
+        # Get all matches where at least one player from this team participated
+        participated_matches = Match.objects.filter(players__team=self).annotate(num_players=Count('players')).filter(num_players__gt=0)
+
+        # Clear current participation and set new participation
+        self.matches_participated.clear()
+        self.matches_participated.add(*participated_matches)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.update_matches_participation()
+    def match_scores(self):
+        match_scores = PlayerScore.objects.filter(player__team=self).values('match__name').annotate(team_score=Sum('score')).order_by('match__name')
+        return match_scores
+    """def total_score(self):
         return sum(player.total_tournament_score() for player in self.players.all())
+    """
+    def team_match_score(self):
+        return sum(player.match_scores() for player in self.players.all())
 
     def player_list(self):
         return ", ".join(player.player_name for player in self.players.all())
@@ -22,6 +48,8 @@ class Player(models.Model):
     email = models.EmailField(unique=True)
     phone_no = models.CharField(max_length=15)
     college_name = models.CharField(max_length=100)
+    def match_scores(self):
+        return self.scores.all()
 
     def total_tournament_score(self):
         return self.scores.aggregate(total=Sum('score'))['total'] or 0
@@ -29,20 +57,26 @@ class Player(models.Model):
     def __str__(self):
         return f"{self.player_name} ({self.ign})"
 
-class Match(models.Model):
-    name = models.CharField(max_length=100)
-    date = models.DateField()
 
-    def __str__(self):
-        return self.name
 
 class PlayerScore(models.Model):
     player = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='scores')
     match = models.ForeignKey(Match, on_delete=models.CASCADE, related_name='scores')
     score = models.IntegerField()
-
+    
     class Meta:
         unique_together = ('player', 'match')
 
     def __str__(self):
         return f"{self.player.player_name} - {self.match.name} - {self.score}"
+
+"""class MatchScore(models.Model):
+    match = models.ForeignKey(Match, on_delete=models.CASCADE, related_name='match_scores')
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='match_scores')
+    score = models.IntegerField()
+
+    class Meta:
+        unique_together = ('match', 'team')
+
+    def __str__(self):
+        return f"{self.match.name} - {self.team.name} - {self.score}" """
